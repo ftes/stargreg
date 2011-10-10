6d6babaa416d2ef8a72da12f18d16e4d5ebe1aa0
Fredrik Teschke <checker.0@gmail.com>
2011-10-10 14:48:58 +0200

    setLaufendeKosten() Stub hinzugefügt

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalMarkt.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalMarkt.java
@@ -1,9 +1,16 @@
 package de.dhbw.stargreg.code;
 
+import java.util.HashMap;
+
 /**
  * 
  * @author fredrik
  *
  */
 public class PersonalMarkt extends Markt {
+
+	public void setLaufendeKosten(HashMap<PersonalTyp, Double> personalLaufendeKosten) {
+		// TODO Auto-generated method stub
+		
+	}
 }
