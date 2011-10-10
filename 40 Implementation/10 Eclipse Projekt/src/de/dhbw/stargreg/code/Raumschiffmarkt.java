832b6a66858fa7f8aaee24f5cc49ca9a74d572c9
Fredrik Teschke <checker.0@gmail.com>
2011-10-10 11:53:50 +0200

    JavaDoc für Raumschiffmarkt gepflegt.

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/Raumschiffmarkt.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/Raumschiffmarkt.java
@@ -9,25 +9,51 @@ import de.dhbw.stargreg.util.Gruppierung;
 import de.dhbw.stargreg.util.Util;
 
 /**
+ * Beschreibt den Raumschiffmarkt, der für die Spieler den Absatzmarkt darstellt
+ * Die Absatzmengen sind abhängig von den jeweiligen Angebots-Mengen und Preisen,
+ * und werden zusätzlich mit einem als sinnvoll angenommenem Vielfachen der Bauteilkosten
+ * für ein Raumschiff verglichen, um zu hohe Preise zu verhindern
  * 
  * @author fredrik
  *
  */
 public class Raumschiffmarkt {
 
+	/**
+	 * Beschreibt die Grundnachfrage nach jedem Raumschifftyp in dieser Periode
+	 */
 	private HashMap<Raumschifftyp, Integer> nachfragen = new HashMap<Raumschifftyp, Integer>();
-	private Vector<Raumschifftyp> raumschifftypen = new Vector<Raumschifftyp>();
 	
+	/**
+	 * Liste mit allen Raumschifftypen
+	 */
+	private final Vector<Raumschifftyp> raumschifftypen = new Vector<Raumschifftyp>();
+	
+	/**
+	 * Fuegt einen Raumschifftyp zur Liste hinzu
+	 * @param typ Hinzuzufügender Raumschifftyp
+	 */
 	public void fuegeRaumschifftypHinzu(Raumschifftyp typ) {
 		this.raumschifftypen.add(typ);
 		System.out.printf("%s zum Raumschiffmarkt hinzugefügt\n", typ);
 	}
 
+	/**
+	 * Setzt die Nachfrage für den gegebenen Raumschifftyp
+	 * @param typ Raumschifftyp
+	 * @param nachfrage Nachfragemenge für {@code typ}
+	 */
 	public void setNachfrage(Raumschifftyp typ, int nachfrage) {
 		this.nachfragen.put(typ, nachfrage);			//wenn bereits vorhanden, dann überschreibt dies den alten Wert
 		System.out.printf("Nachfrage nach %s zum Raumschiffmarkt hinzugefügt: %d Stück\n", typ, nachfrage);
 	}
 	
+	/**
+	 * Berechnet die Absatzmengen für die jeweiligen Spieler (Angebote) für diese Periode
+	 * @param raumschifftyp Raumschifftyp, für den die Absatzmengen bestimmt werden sollen (wichtig wegen der Elementarkosten)
+	 * @param angebote {@code Vector} mit Angeboten der Unternehmen, die Menge und Preis beinhalten
+	 * @return Rückgabe eines {@code Vector} mit allen {@code Verkauf}-Objekten, die die jeweilige Absatzmenge enthalten
+	 */
 	public Vector<Verkauf> berechneTypAbsatz(Raumschifftyp raumschifftyp, Vector<Angebot> angebote) {
 		//To-Do: Was passiert bei gleichen Preisen
 		//To-Do: Beachten, dass maximal verfügbare Gesamtmenge auch verkauft wird
@@ -75,6 +101,12 @@ public class Raumschiffmarkt {
 		return verkaeufe;
 	}
 
+	/**
+	 * Gruppiert alle Angebote nach Raumschifftypen, berechnet für diese jeweils mit {@code berechneTypAbsatz()} den Absatz für die
+	 * jeweiligen Unternehmen und gibt alle Verkäufe in einem {@code Vector} zurück
+	 * @param angebote {@code Vector} mit allen Angeboten aller Spieler zu allen Raumschifftypen
+	 * @return {@code Vector} mit allen Verkäufen
+	 */
 	public Vector<Verkauf> berechneGesamtAbsatz(Vector<Angebot> angebote) {
 		Vector<Verkauf> verkaeufe = new Vector<Verkauf>();
 		
