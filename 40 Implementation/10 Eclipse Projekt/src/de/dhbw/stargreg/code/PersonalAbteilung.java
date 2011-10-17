778bfbe74edecfcf548e192402bbade73c86dc39
Britstift <britta.jochum@yahoo.de>
2011-10-17 19:35:45 +0200

    Brittas Änderungen Personal

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
@@ -8,20 +8,24 @@ import java.util.HashMap;
  *
  */
 public class PersonalAbteilung extends Abteilung {
-	private double kosten_Mit1 = 50.0;
-	private double kosten_Mit2 = 100.0;
-	private double kosten_Mit3 = 200.0;
+	private double kosten_r2d2             = 50.0;
+	private double kosten_kampfdroide      = 100.0;
+	private double kosten_droideka         = 200.0;
 	
 	private int summeMit = 0;
-	double werbKosten = 0.0;
-
 	private double personalkosten = 0.0;
 	
 	private HashMap<PersonalTyp, Integer> allePersonaltypen = new HashMap<PersonalTyp, Integer>();	
 
-	public double erwerbenMit (PersonalTyp p, int anzahlMit ){
-		//Erwerbskosten berrechnen
-		return werbKosten;
+	public void erwerbenMit (PersonalTyp p, PersonalTyp q, int anzahlMit ){
+		int anzahlMit_p = 0;
+		int anzahlMit_q = 0;
+				
+		anzahlMit_q = anzahlMit_q + anzahlMit;
+		anzahlMit_p = anzahlMit_p - anzahlMit;
+		
+		allePersonaltypen.put(p, anzahlMit_p);
+		allePersonaltypen.put(q, anzahlMit_q);
 	}//erwerbenMit
 	
 	public void wegnehmenMit (int anzahlMit){
