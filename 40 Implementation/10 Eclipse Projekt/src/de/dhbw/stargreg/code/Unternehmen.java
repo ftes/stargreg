1c028e1ec57e1645e9bbb7aefe81a20bb771d522
Fredrik Teschke <checker.0@gmail.com>
2011-10-17 10:23:20 +0200

    Spiel simulieren() erweitert

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/Unternehmen.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/Unternehmen.java
@@ -6,12 +6,14 @@ package de.dhbw.stargreg.code;
  *
  */
 public class Unternehmen {
-	private String name;
-	private EinkaufsAbteilung einkauf = new EinkaufsAbteilung();
-	private ProduktionsAbteilung produktion = new ProduktionsAbteilung();
-	private VerkaufsAbteilung verkauf = new VerkaufsAbteilung();
-	private FinanzAbteilung finanzen = new FinanzAbteilung();
-	private PersonalAbteilung personal = new PersonalAbteilung();
+	private final String name;
+	private final EinkaufsAbteilung einkauf = new EinkaufsAbteilung();
+	private final ProduktionsAbteilung produktion = new ProduktionsAbteilung();
+	private final VerkaufsAbteilung verkauf = new VerkaufsAbteilung();
+	private final FinanzAbteilung finanzen = new FinanzAbteilung();
+	private final PersonalAbteilung personal = new PersonalAbteilung();
+	
+	private boolean rundeEingecheckt = false;
 	
 	public Unternehmen(String name) {
 		this.name = name;
@@ -19,5 +21,18 @@ public class Unternehmen {
 
 	public String toString() {
 		return name;
+	}
+	
+	public boolean getRundeEingecheckt() {
+		return rundeEingecheckt;
+	}
+	
+	public void rundeEinchecken() {
+		System.out.printf("%s hat Runde eingecheckt\n", this);
+		rundeEingecheckt = true;
+	}
+	
+	public void simuliere() {
+		rundeEingecheckt = false;
 	}
 }
