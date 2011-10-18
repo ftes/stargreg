cde05ed4d9b5479c66045d0541edab273fe40be9
Britstift <britta.jochum@yahoo.de>
2011-10-18 12:41:53 +0200

    Änderunge Personal

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
@@ -3,7 +3,7 @@ package de.dhbw.stargreg.code;
 import java.util.HashMap;
 
 /**
- * Hier werden die Mitarbeiter der drei Qualitätsstufen über den gemeinsamen Obertyp 'Personaltyp' 
+ * Hier werden die Personal der drei Qualitätsstufen über den gemeinsamen Obertyp 'Personaltyp' 
  * verwaltet. Das Personal kann erworben bzw. eingestellt, entlassen und geschult werden.  
  * @author Britta
  *
@@ -18,7 +18,7 @@ public class PersonalAbteilung extends Abteilung {
 	/**
 	 * Erzeugt eine Personalabteilung. Eine Anfangsmenge Personal ist war angedacht, sollte aber nicht Ã¼ber
 	 * den Konstruktor eingepflegt werden, da dieser Parameter dann Ã¼ber Unternehmen() weitergereicht werden
-	 * mÃ¼sste. Stattdessen wird nach Erzeugung eines Unternehmens die Mitarbeiter-Menge per {@code einstellen()}
+	 * mÃ¼sste. Stattdessen wird nach Erzeugung eines Unternehmens die Personal-Menge per {@code einstellen()}
 	 * erweitert.
 	 */
 	public PersonalAbteilung(Unternehmen unternehmen) {
@@ -26,11 +26,11 @@ public class PersonalAbteilung extends Abteilung {
 	}//Konstruktor
 	
 	/**
-	 * Erwirbt und stellt eine Anzahl an Mitarbeitern in einer bestimmten Qualitätsstufe ein. 
-	 * Die Anzahl aller Mitarbeiter und die neuen laufendeKosten werden gespeichert; die 
+	 * Erwirbt und stellt eine Anzahl an Personaln in einer bestimmten Qualitätsstufe ein. 
+	 * Die Anzahl aller Personal und die neuen laufendeKosten werden gespeichert; die 
 	 * Werbungskosten werden abgebucht.
-	 * @param p Qualitätsstufe der einzustellenden Mitarbeiter
-	 * @param anzahlPers Anzahl der einzustellenden Mitarbeiter 
+	 * @param p Qualitätsstufe der einzustellenden Personal
+	 * @param anzahlPers Anzahl der einzustellenden Personal 
 	 */
 	public void erwerbenMit (PersonalTyp p, int anzahlPers, FinanzAbteilung konto){
 		double neuePersKosten = laufendeKosten += (p.getLaufendeKosten()+ p.getWerbungsKosten() )*anzahlPers;
@@ -44,62 +44,37 @@ public class PersonalAbteilung extends Abteilung {
 
 		laufendeKosten += p.getLaufendeKosten() * anzahlPers;
 		anzahlPersonal += anzahlPers;
-		
-		
 	}//erwerbenMit
 	
 	/**
-	 * Entlässt eine Anzahl an Mitarbeitern in einer bestimmten Qualitätsstufe. Die Änderungen an
-	 * der Gesamtmitarbeiteranzahl und den laufendeKosten werden gespeichert.
-	 * @param p Qualitätsstufe der zu entlassenen Mitarbeiter
-	 * @param anzahlPers Anzahl der zu entlassenen Mitarbeiter
+	 * Entlässt eine Anzahl an Personal in einer bestimmten Qualitätsstufe. Die Änderungen an
+	 * der gesamten Personalanzahl und den laufenden Kosten werden gespeichert.
+	 * @param p personalTyp von Personaltyp
+	 * @param anzahlPers Anzahl des zu entlassenen Personals
 	 */
-	public void entlassenMit (PersonalTyp p, int anzahlPers){
-		int anzahlPers_p = personal.get(p);
-		personal.put(p, (anzahlPers_p - anzahlPers));
-		anzahlPersonal -= anzahlPers;
-		laufendeKosten -= p.getLaufendeKosten()* anzahlPers;
-	}//entlassenMit
+	public void entlassen (PersonalTyp personalTyp, int anzahl){
+		int anzahlPers = personal.get(personalTyp);
+		personal.put(personalTyp, (anzahlPers - anzahl));
+		anzahlPersonal -= anzahl;
+		laufendeKosten -= personalTyp.getLaufendeKosten()* anzahl;
+	}//entlassen
 
 	/**
-<<<<<<< HEAD
-	 * Schult Mitarbeiter, d.h. eine Anzahl an Mitarbietern werden aus ihrer aktuellen Qualitätsstufe
-	 * entnommen und der nächsten Stufe hinzugefügt. Dabei kann die Qualität immer nur um eine Stufe erhöht
-	 * werden. Die Änderungen der laufendeKosten durch die neuen laufenden Kostenwerden gespeichert;
-	 * die Schulungskosten werdne abgebucht.
-	 * @param p Ist-Qualitätsstufe
-	 * @param q Soll-Qualitätsstufe 
-	 * @param anzahlPers Anzahl der zu schulenden Mitarbeiter
+	 * Schult Personal, d.h. eine Anzahl an Personal wird aus ihrer aktuellen Qualitätsstufe
+	 * entnommen und der nächsten Stufe hinzugefügt. Dabei kann die Qualität immer nur um eine 
+	 * Stufe erhöht werden. Die Änderungen der laufenden Kosten durch die neuen Kosten werden 
+	 * gespeichert; Schulungskosten werden abgebucht.
+	 * @param von Ist-Qualitätsstufe
+	 * @param nach Soll-Qualitätsstufe 
+	 * @param anzahlPers Anzahl der zu schulenden Personal
 	 */
-	public void schulenMit (PersonalTyp p, PersonalTyp q, int anzahlPers, FinanzAbteilung konto ){ 
+	public void schulen (PersonalTyp von, PersonalTyp nach, int anzahl, FinanzAbteilung konto ){ 
 		// ToDo: Parameter q wegnehmen; "schulbar-Prüfung" ergännzen
-		double neuePersKosten = (q.getLaufendeKosten()-p.getLaufendeKosten() )* anzahlPers;
-		if (konto.getKontostand() < (p.getSchulungsKosten() + neuePersKosten) ) {
+		double neuePersKosten = (nach.getLaufendeKosten()-von.getLaufendeKosten() )* anzahl;
+		if (konto.getKontostand() < (von.getSchulungsKosten() + neuePersKosten) ) {
 			// Kapital zu gering!
 			return;
 		}
-		
-		int anzahlPers_p = personal.get(p);
-		int anzahlPers_q = personal.get(q);
-		anzahlPers_q = anzahlPers_q + anzahlPers;
-		anzahlPers_p = anzahlPers_p - anzahlPers;
-		
-		personal.put(p, anzahlPers_p);
-		personal.put(q, anzahlPers_q);
-		konto.abbuchen(p.getSchulungsKosten()*anzahlPers);
-		//laufendeKosten neu berechnen
-		laufendeKosten += neuePersKosten;
-		
-		/**
-		 * Schult Mitarbeiter, d.h. entnimmt Mitarbieter aus der aktuellen Kategorie und fuegt
-		 * sie in die nächste hoehere Qualitätsstufe ein. Eine Schulung kann immer nur um eine 
-		 * Qualitätsstufe erhöhen. Die laufendeKosten werden neu berrechnet.
-		 * @param von Ist-Qualitätsstufe
-		 * @param nach Soll-Qualitätsstufe
-		 * @param anzahl Anzahl des zu schulenden Personals
-		 */
-	public void schulen (PersonalTyp von, PersonalTyp nach, int anzahl){
-		//Was ist mit den Schulungskosten?!
 		int anzahlVonAlt = personal.get(von);
 		int anzahlNachAlt = personal.get(nach);
 		anzahlNachAlt = anzahlNachAlt + anzahl;
@@ -107,23 +82,11 @@ public class PersonalAbteilung extends Abteilung {
 		
 		personal.put(von, anzahlVonAlt);
 		personal.put(nach, anzahlNachAlt);
+		konto.abbuchen(von.getSchulungsKosten()*anzahl);
 		//laufendeKosten neu berechnen
-		laufendeKosten -= von.getLaufendeKosten()* anzahl;
-		laufendeKosten += nach.getLaufendeKosten()* anzahl;
-	}//erwerbenMit
-	
-	/**
-	 * EntlÃ¤sst Mitarbeiter (Name angepasst an Gesamt-Terminologie)
-	 * @param personalTyp PersonalTyp
-	 * @param anzahl Anzahl der Mitarbeiter
-	 */
-	public void entlassen (PersonalTyp personalTyp, int anzahl){
-		int anzahlPers_p = personal.get(personalTyp);
-		personal.put(personalTyp, (anzahlPers_p - anzahl));
-		anzahlPersonal -= anzahl;
-		laufendeKosten -= personalTyp.getLaufendeKosten()* anzahl;
-	}//wegnehmenMit
-	
+		laufendeKosten += neuePersKosten;
+	}
+/*	
 	//nicht auf einmal Parameter umdrehen!
 	public void einstellen (PersonalTyp personalTyp, int anzahl){
 		//Werbungskosten?!
@@ -131,8 +94,8 @@ public class PersonalAbteilung extends Abteilung {
 		personal.put (personalTyp, (anzahlAlt + anzahl));
 		anzahlPersonal += anzahl;
 		laufendeKosten += personalTyp.getLaufendeKosten()* anzahl;
-	}//schulenMit
-	
+	}
+*/	
 	public int getAnzahlPersonal (){
 		return anzahlPersonal;
 	}//getMitAnzahl
