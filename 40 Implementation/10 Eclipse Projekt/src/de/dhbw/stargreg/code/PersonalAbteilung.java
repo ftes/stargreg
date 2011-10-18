b959bf87be03e027eaefd5796c9ee22eccc35cb6
Fredrik Teschke <fredrikteschke@gmail.com>
2011-10-18 12:47:51 +0200

    PersonalAbteilung um Kontoüberprüfungen und Mengenüberprüfungen erweitert

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/PersonalAbteilung.java
@@ -34,39 +34,69 @@ public class PersonalAbteilung extends Abteilung {
 	 * @param nach n�chste h�here Kategorie
 	 * @param anzahl Anzahl der zu schulenden Mitarbeiter
 	 */
-	public void schulen (PersonalTyp von, PersonalTyp nach, int anzahl){
-		//Was ist mit den Schulungskosten?!
-		int anzahlVonAlt = personal.get(von);
-		int anzahlNachAlt = personal.get(nach);
-		anzahlNachAlt = anzahlNachAlt + anzahl;
-		anzahlVonAlt = anzahlVonAlt - anzahl;
+	public boolean schulen (PersonalTyp von, int anzahl) {
+		if (personal.get(von) < anzahl) {
+			System.err.printf("Weniger als %d %s zum schulen vorhanden\n", anzahl, von);
+			return false;
+		}
 		
-		personal.put(von, anzahlVonAlt);
-		personal.put(nach, anzahlNachAlt);
+		PersonalTyp nach = von.getNaechsterPersonalTyp();
+		Schulung schulung = new Schulung(von, unternehmen, anzahl, von.getSchulungsKosten());
+		if (! unternehmen.getFinanzen().abbuchen(schulung.getKosten())) {
+			System.err.printf("Nicht genug Geld, um %d %s zu schulen\n", anzahl, von);
+			return false;
+		}
+		
+		int anzahlVon = personal.get(von) - anzahl;
+		int anzahlNach = personal.get(nach) + anzahl;
+		personal.put(von, anzahlVon);
+		personal.put(nach, anzahlNach);
 		//Personalkosten neu berechnen
-		laufendeKosten -= von.getLaufendeKosten()* anzahl;
-		laufendeKosten += nach.getLaufendeKosten()* anzahl;
-	}//erwerbenMit
+		laufendeKosten -= von.getLaufendeKosten() * anzahl;
+		laufendeKosten += nach.getLaufendeKosten() * anzahl;
+		
+		Spiel.INSTANCE.getPersonalMarkt().fuegeTransaktionHinzu(schulung);
+		return true;
+	}
 	
 	/**
 	 * Entlässt Mitarbeiter (Name angepasst an Gesamt-Terminologie)
 	 * @param personalTyp PersonalTyp
 	 * @param anzahl Anzahl der Mitarbeiter
 	 */
-	public void entlassen (PersonalTyp personalTyp, int anzahl){
-		int anzahlMit_p = personal.get(personalTyp);
-		personal.put(personalTyp, (anzahlMit_p - anzahl));
+	public boolean entlassen (PersonalTyp personalTyp, int anzahl){
+		int vorhanden = personal.get(personalTyp);
+		
+		if (vorhanden < anzahl) {
+			System.err.printf("Weniger als %d %s zum entlassen vorhanden\n", anzahl, personalTyp);
+			return false;
+		}
+		
+		personal.put(personalTyp, (vorhanden - anzahl));
 		anzahlPersonal -= anzahl;
 		laufendeKosten -= personalTyp.getLaufendeKosten()* anzahl;
+		
+		Entlassung entlassung = new Entlassung(personalTyp, unternehmen, anzahl);
+		Spiel.INSTANCE.getPersonalMarkt().fuegeTransaktionHinzu(entlassung);
+		return true;
 	}//wegnehmenMit
 	
 	//nicht auf einmal Parameter umdrehen!
-	public void einstellen (PersonalTyp personalTyp, int anzahl){
-		//Werbungskosten?!
-		int anzahlAlt = personal.get(personalTyp);
-		personal.put (personalTyp, (anzahlAlt + anzahl));
+	public boolean einstellen (PersonalTyp personalTyp, int anzahl){
+		Einstellung einstellung = new Einstellung(personalTyp, unternehmen, anzahl, personalTyp.getWerbungsKosten());
+		
+		if (! unternehmen.getFinanzen().abbuchen(einstellung.getKosten())) {
+			System.err.printf("Nicht genug Geld, um %d %s einzustellen\n", anzahl, personalTyp);
+			return false;
+		}
+		
+		int vorhanden = personal.get(personalTyp);
+		personal.put (personalTyp, (vorhanden + anzahl));
 		anzahlPersonal += anzahl;
 		laufendeKosten += personalTyp.getLaufendeKosten()* anzahl;
+		
+		Spiel.INSTANCE.getPersonalMarkt().fuegeTransaktionHinzu(einstellung);
+		return true;
 	}//schulenMit
 	
 	public int getAnzahlPersonal (){
@@ -82,7 +112,7 @@ public class PersonalAbteilung extends Abteilung {
 	}//getPersonalkosten
 	
 	public double getLaufendeKosten (PersonalTyp personalTyp) {
-		return personalTyp.getLaufendeKosten()*personal.get(personalTyp);
+		return personalTyp.getLaufendeKosten() * personal.get(personalTyp);
 	}//getTypPerosnalkosten
 	
 	public double getDurchschnittlicheQualitaet() {
