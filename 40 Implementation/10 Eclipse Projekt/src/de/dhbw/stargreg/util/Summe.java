f1930cf4c472ac564bd3804f6446d841fa0de072
Fredrik Teschke <fredrikteschke@gmail.com>
2011-10-25 22:51:48 +0200

    Hilfsfunktionen zum Sortieren und zum Summieren

---
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/util/Summe.java
@@ -0,0 +1,5 @@
+package de.dhbw.stargreg.util;
+
+public interface Summe<T> {
+	public double von(T object);
+}
--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/util/Util.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/util/Util.java
@@ -1,8 +1,11 @@
 package de.dhbw.stargreg.util;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Vector;
 
+import de.dhbw.stargreg.code.RaumschiffTyp;
 import de.dhbw.stargreg.code.Unternehmen;
 import de.dhbw.stargreg.code.Verkauf;
 
@@ -42,13 +45,90 @@ public class Util {
 		});
 	}
 	
-	public static <T> Vector<T> filtereVector(Vector<T> vector, Filter<T> filter) {
-		Vector<T> ergebnis = new Vector<T>();
-		for (T object : vector) {
+	public static <O> Vector<O> filtereVector(Vector<O> vector, Filter<O> filter) {
+		Vector<O> ergebnis = new Vector<O>();
+		for (O object : vector) {
 			if (filter.nach(object)) {
 				ergebnis.add(object);
 			}
 		}
 		return ergebnis;
+	}
+	
+	public static <T> double summiereVector(Vector<T> vector, Summe<T> summe) {
+		double tmp = 0;
+		for (T object : vector) {
+			tmp += summe.von(object);
+		}
+		return tmp;		
+	}
+	
+	public static double summiereVerkaeufe(Vector<Verkauf> verkaeufe) {
+		return summiereVector(verkaeufe, new Summe<Verkauf>() {
+			public double von(Verkauf verkauf) {
+				return verkauf.getKosten();
+			}
+		});
+	}
+	
+	public static double summiereHashMap(HashMap<?, Double> hashMap) {
+		Vector<Double> vector = new Vector<Double>(hashMap.values());
+		return summiereVector(vector, new Summe<Double>() {
+			public double von(Double dbl) {
+				return dbl;
+			}
+		});
+	}
+	
+	public static <G, O> HashMap<G, Double> gruppiereUndSummiereVector(Vector<O> vector, Gruppierung<G, O> gruppierung, Summe<O> summe) {
+		HashMap<G, Vector<O>> gruppiert = gruppiereVector(vector, gruppierung);
+		HashMap<G, Double> summiert = new HashMap<G, Double>();
+		for (G gruppe : gruppiert.keySet()) {
+			summiert.put(gruppe, summiereVector(gruppiert.get(gruppe), summe));
+		}
+		return summiert;
+	}
+	
+	public static HashMap<Unternehmen, Double> gruppiereUndSummiereVerkaeufeNachUnternehmen(Vector<Verkauf> verkaeufe) {
+		return gruppiereUndSummiereVector(
+				verkaeufe,
+				new Gruppierung<Unternehmen, Verkauf>() {
+					public Unternehmen nach(Verkauf verkauf) {
+						return verkauf.getUnternehmen();
+					}
+				},
+				new Summe<Verkauf>() {
+					public double von(Verkauf verkauf) {
+						return verkauf.getKosten();
+					}
+				}
+		);
+	}
+	
+	public static HashMap<RaumschiffTyp, Double> gruppiereUndSummiereVerkaeufeNachRaumschiffTyp(Vector<Verkauf> verkaeufe) {
+		return gruppiereUndSummiereVector(
+				verkaeufe,
+				new Gruppierung<RaumschiffTyp, Verkauf>() {
+					public RaumschiffTyp nach(Verkauf verkauf) {
+						return verkauf.getRaumschiffTyp();
+					}
+				},
+				new Summe<Verkauf>() {
+					public double von(Verkauf verkauf) {
+						return verkauf.getKosten();
+					}
+				}
+		);
+	}
+	
+	public static <G> Vector<G> sortiere(final HashMap<G, Double> hashMap, boolean aufsteigend) {
+		Vector<G> vector = new Vector<G>(hashMap.keySet());
+		Collections.sort(vector, new Comparator<G>() {
+			public int compare(G arg1, G arg2) {
+				return hashMap.get(arg1).compareTo(hashMap.get(arg2));
+			}
+		});
+		if (! aufsteigend) Collections.reverse(vector);
+		return vector;
 	}
 }
