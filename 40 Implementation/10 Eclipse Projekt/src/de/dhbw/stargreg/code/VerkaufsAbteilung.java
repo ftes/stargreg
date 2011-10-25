229a62848a838204a65994357cdeebc4ad0c8383
Fredrik Teschke <fredrikteschke@gmail.com>
2011-10-25 10:33:01 +0200

    verkaufe() implementiert

--- 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/VerkaufsAbteilung.java
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/code/VerkaufsAbteilung.java
@@ -13,9 +13,23 @@ public class VerkaufsAbteilung extends Abteilung {
 		super(unternehmen);
 	}
 
+	/**
+	 * Verkauft Raumschiffe in den übergebenen Mengen. Dabei werden die Raumschiffe aus dem Lager
+	 * entnommen und der Ertrag wird dem Konto gutgeschrieben.
+	 * @param verkaeufe {@code Vector} mit den zu tätigenden Verkäufen.
+	 */
 	public void verkaufe(Vector<Verkauf> verkaeufe) {
-		// TODO Auto-generated method stub
-		
+		for (Verkauf verkauf : verkaeufe) {
+			if (verkauf.getUnternehmen() != unternehmen) {
+				System.err.printf("Nicht für %s bestimmte Verkäufe zum verkaufen erhalten\n", unternehmen);
+				return;
+			}
+			if (! unternehmen.getLager().entnehmen(verkauf.getRaumschiffTyp(), verkauf.getMenge())) {
+				System.err.printf("Weniger als %d %s vorhanden, Fehler in Verkäufen\n", verkauf.getMenge(), verkauf.getRaumschiffTyp());
+				return;
+			}
+			unternehmen.getFinanzen().einzahlen(verkauf.getKosten());
+		}
 	}
 
 }
