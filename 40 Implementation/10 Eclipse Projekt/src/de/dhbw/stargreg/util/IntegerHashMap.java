62500fa779a07d931a9503742b17b09db3f795e6
Fredrik Teschke <fredrikteschke@gmail.com>
2011-10-25 09:53:25 +0200

    IntegerHashMap für einfacheren Umgang, mit verbesserter get() und neuer add() und subtract()

---
+++ 40 Implementation/10 Eclipse Projekt/src/de/dhbw/stargreg/util/IntegerHashMap.java
@@ -0,0 +1,33 @@
+package de.dhbw.stargreg.util;
+
+import java.util.HashMap;
+
+/**
+ * Vereinfachte HashMap zum Umgang mit Integers als value-Elemente.
+ * Die get-Methode wurde überschrieben, sodass bei Nicht-Vorhandensein
+ * des Keys stall {@code null} 0 zurückgeliefert wird, und es gibt nun
+ * eine {@code add}-Methode.
+ * @author fredrik
+ *
+ * @param <K>
+ */
+public class IntegerHashMap<K> extends HashMap<K, Integer> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Integer get(Object key) {
+		if (! super.containsKey(key)) return 0;
+		return super.get(key);
+	}
+	
+	public void add(K key, Integer value) {
+		super.put(key, super.get(key) + value);
+	}
+	
+	public boolean subtract(K key, Integer value) {
+		if (super.get(key) - value < 0) return false;
+		super.put(key, super.get(key) - value);
+		return true;
+	}
+
+}
