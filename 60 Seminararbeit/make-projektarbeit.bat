7b93d2a84b6c57aaaf390a45e8e052963be75d3c
Fredrik Teschke <checker.0@gmail.com>
2011-11-06 20:15:16 +0100

    Spiellogik Einleitung und Bauteilpreise

--- 60 Seminararbeit/make-projektarbeit.bat
+++ 60 Seminararbeit/make-projektarbeit.bat
@@ -5,31 +5,17 @@ echo Parameter: Name der tex-datei OHNE die Endung .tex
 set file=80_seminararbeit
 echo %file%%
 
-pdflatex %file%.tex
+pdflatex -shell-escape %file%.tex
 bibtex %file%
 makeindex -s %file%.ist -t %file%.alg -o %file%.acr %file%.acn
 makeindex -s %file%.ist -t %file%.glg -o %file%.gls %file%.glo
 makeindex -s %file%.ist -t %file%.slg -o %file%.syi %file%.syg
-pdflatex %file%.tex
-pdflatex %file%.tex
-pdflatex %file%.tex
+pdflatex -shell-escape %file%.tex
+pdflatex -shell-escape %file%.tex
+pdflatex -shell-escape %file%.tex
 
-del %file%.acn
-del %file%.acr
-del %file%.alg
-del %file%.aux
-del %file%.bbl
-del %file%.blg
-del %file%.glg
-del %file%.glo
-del %file%.gls
-del %file%.ist
-del %file%.lof
-del %file%.log
-del %file%.lop
-del %file%.lot
-del %file%.slg
-del %file%.syg
-del %file%.syi
-del %file%.toc
-del %file%.url
+REM ren %file%.tex tmp%file%.tex
+REM ren %file%.pdf tmp%file%.pdf
+REM del %file%.*
+REM ren tmp%file%.tex %file%.tex
+REM ren tmp%file%.pdf %file%.pdf
