58f9f03712c3bb319df5571978478f380ddf6748
Fredrik Teschke <checker.0@gmail.com>
2011-11-06 01:31:11 +0100

    Seminararbeit Vorlage eingepflegt

--- 60 Seminararbeit/make-projektarbeit.bat
+++ 60 Seminararbeit/make-projektarbeit.bat
@@ -8,3 +8,24 @@ makeindex -s %1.ist -t %1.glg -o %1.gls %1.glo
 makeindex -s %1.ist -t %1.slg -o %1.syi %1.syg
 pdflatex %1.tex
 pdflatex %1.tex
+pdflatex %1.tex
+
+del %1.acn
+del %1.acr
+del %1.alg
+del %1.aux
+del %1.bbl
+del %1.blg
+del %1.glg
+del %1.glo
+del %1.gls
+del %1.ist
+del %1.lof
+del %1.log
+del %1.lop
+del %1.lot
+del %1.slg
+del %1.syg
+del %1.syi
+del %1.toc
+del %1.url
