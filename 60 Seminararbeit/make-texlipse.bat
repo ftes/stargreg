3a2f6a9d8e454cf7f071720e69926f140957116f
Fredrik Teschke <checker.0@gmail.com>
2011-11-13 22:29:15 +0100

    Seminararbeit Abschluss

--- 60 Seminararbeit/make-texlipse.bat
+++ 60 Seminararbeit/make-texlipse.bat
@@ -6,13 +6,13 @@ SET tex=%1
 SET name=%tex:~0,-4%
 
 
-REM pdflatex %name%.tex
+pdflatex %name%.tex
 bibtex %name%
 makeindex -s %name%.ist -t %name%.alg -o %name%.acr %name%.acn
-REM makeindex -s %name%.ist -t %name%.glg -o %name%.gls %name%.glo
-REM makeindex -s %name%.ist -t %name%.slg -o %name%.syi %name%.syg
-REM pdflatex %name%.tex
-REM pdflatex %name%.tex
+makeindex -s %name%.ist -t %name%.glg -o %name%.gls %name%.glo
+makeindex -s %name%.ist -t %name%.slg -o %name%.syi %name%.syg
+pdflatex %name%.tex
+pdflatex %name%.tex
 pdflatex -synctex=-1 %name%.tex -interaction=nonstopmode --src-specials -shell-escape %name%.tex
 
 
